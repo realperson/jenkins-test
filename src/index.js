47a0ff89933399867846b305cf9c4bcba9b03eaa
realperson <1750698766@qq.com>
2018-05-07 14:41:45 +0800

    test it

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
   var element = document.createElement('div');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack', 'he he'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', 'he he,test it'], ' ');
 
   return element;
 }
